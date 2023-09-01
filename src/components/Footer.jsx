65ebae96fbc1e0dcdd5435ce6eb37d8b24e16058
Geoffrey Mokua <arisimokua280@gmail.com>
2023-09-01 15:44:47 +0300

    added logimage to the navbar

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ function Footer() {
           <div className="footer-content">
             <ul className="footer-content__1">
               <li>
-                <span>CAR</span> SOKO
+                <span>CAR</span> Rental
               </li>
               <li>
                 We offers a big range of vehicles for all your driving needs. We
